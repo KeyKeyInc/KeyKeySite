6317bb328b38cead324b17ca9725beae89d99045
marcoigorr <petronio.mrc@gmail.com>
2022-08-08 18:37:28 +0200

    Finito menu a scomparsa aggiunte modifiche per desktop e mobile

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 
 window.onload = function() {
     const menu_btn = document.querySelector('.burger');
+    const mobile_menu = document.querySelector('.mobile-nav');
 
     menu_btn.addEventListener('click', function() {
         menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
     });
+
+
 }
