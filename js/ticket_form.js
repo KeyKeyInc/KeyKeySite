22929d9f3f4ecef4481a1571f7dead4184082a63
marcoigorr <petronio.mrc@gmail.com>
2022-08-09 16:58:50 +0200

    Features

--- js/ticket_form.js
+++ js/ticket_form.js
@@ -8,15 +8,18 @@ form.addEventListener('submit', (e) => {
 
 window.onload = function() {
     var columns = document.querySelectorAll('.column');
+    var info = document.querySelectorAll('.info')
 
     for (let i = 0; i < columns.length; i++) {
         columns[i].addEventListener('click', function() {
             columns[i].classList.toggle('selected');
+            info[i].classList.toggle('selected');
 
             for (let j = 0; j < columns.length; j++) {
                 if (j === i) continue;
 
                 columns[j].classList.remove('selected');
+                info[j].classList.remove('selected');
             }
         });
     }    
