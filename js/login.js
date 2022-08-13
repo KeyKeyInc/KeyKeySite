902c322a524471d7b17f69c48dd19514a2bdf11a
marcoigorr <petronio.mrc@gmail.com>
2022-08-13 17:26:23 +0200

    Aggiunto login e reindirizzamenti

---
+++ js/login.js
@@ -0,0 +1,13 @@
+
+const form = document.querySelector('section');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    return false;
+});
+
+const participate_btn = document.querySelector('#redirect');
+
+participate_btn.addEventListener('click', () => {
+    window.location.href = './ticket_form.html';
+});
